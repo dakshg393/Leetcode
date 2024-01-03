751fe071cd883bca22aec97258b070c135e59a26
Daksh Gupta <142725301+dakshg393@users.noreply.github.com>
2024-01-04 00:24:47 +0530

    Time: 0 ms (100%), Space: 6.4 MB (20.08%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+
+        if(n==0){
+            return false;
+        }
+        return (log2(n) == floor(log2(n)));
+
+    }
+};
