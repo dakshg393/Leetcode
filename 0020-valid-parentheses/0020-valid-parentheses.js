4ca62b23112189479c436da870c4a97b4cb2e751
Daksh Gupta <142725301+dakshg393@users.noreply.github.com>
2025-04-18 00:07:02 +0530

    Time: 0 ms (100%), Space: 57.3 MB (17.4%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = []
+    const map = {
+        "(":")",
+        "{":"}",
+        "[":"]"
+    }
+
+    for(const char of s){
+        if(map[char]){
+            stack.push(map[char])
+        }else{
+            if(stack.pop()!== char){
+                return false
+            }
+        }
+
+    }
+    return stack.length === 0;
+};
